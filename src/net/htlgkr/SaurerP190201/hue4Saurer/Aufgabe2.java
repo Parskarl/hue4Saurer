5750a06ecb68dfc2550c5d833de29cc9f5143bc8
Parskarl <pasci17.3.2006@gmail.com>
2022-10-13 20:04:46 +0200

    7. Abgeben

--- src/net/htlgkr/SaurerP190201/hue4Saurer/Aufgabe2.java
+++ src/net/htlgkr/SaurerP190201/hue4Saurer/Aufgabe2.java
@@ -5,28 +5,37 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.*;
 
-public class Aufgabe2
-{
-    public static void main(String[] args)
-    {
-        try{
+public class Aufgabe2 {
+    public static void main(String[] args) {
+        try {
             System.out.println("Bis welche Zahl wollen sie summieren?");
             Scanner s = new Scanner(System.in);
             int weite = Integer.parseInt(s.nextLine());
             int sum = 0;
             List<Callable<Integer>> listofintegerlists = new ArrayList<>();
-            if(weite>=100)
-            {
-                int howmanythreads = weite/100;
+            if (weite >= 100) {
+                int howmanythreads = weite / 100;
                 ExecutorService executor = Executors.newFixedThreadPool(howmanythreads);
-                for(int i = 1; i <= howmanythreads; i++)
-                {
-                    ThreadB gsf = new ThreadB(i*100);
+                for (int i = 1; i <= howmanythreads; i++) {
+                    if (i * 100 > weite) {
+                        ThreadB gsf = new ThreadB(weite);
+                        listofintegerlists.add(gsf);
+                        break;
+                    }
+                    ThreadB gsf = new ThreadB(i * 100);
                     listofintegerlists.add(gsf);
                 }
-                for(Future<Integer> future : executor.invokeAll(listofintegerlists))
-                {
-                    sum += future.get();
+                for (Future<Integer> f : executor.invokeAll(listofintegerlists)) {
+                    sum += f.get();
+                }
+                System.out.println("Ergebnis = " + sum);
+                executor.shutdown();
+            } else {
+                ExecutorService executor = Executors.newFixedThreadPool(1);
+                ThreadB gsf = new ThreadB(weite);
+                listofintegerlists.add(gsf);
+                for (Future<Integer> f : executor.invokeAll(listofintegerlists)) {
+                    sum += f.get();
                 }
                 System.out.println("Ergebnis = " + sum);
                 executor.shutdown();
